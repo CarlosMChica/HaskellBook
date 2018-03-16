a0cd94b94e5065c6cb36c54b1c9bb6a73f551ec1
Carlos Morera de la Chica <carlos@codurance.com>
2018-03-16 16:56:11 +0000

    Continue with Morra exercise

--- chapter26/Morra.hs
+++ chapter26/Morra.hs
@@ -1,12 +1,16 @@
 {-# LANGUAGE TupleSections #-}
 module Morra where
 
+import           Control.Applicative
+import           Control.Monad
 import           Control.Monad.Trans.State
 import           Data.Int
+import           Data.List
 import           Data.Semigroup
 import           System.Random
 
-data Player = Human | AI
+data Side = Odds | Evens deriving (Read, Eq)
+data Player = Human { side :: Side} | AI { side :: Side}
 data Hand = Hand {
     player  :: Player,
     fingers :: Int
@@ -16,29 +20,68 @@ data Turn = Turn {
     ai    :: Hand
   }
 
-winsTurn :: Turn -> Player
-winsTurn turn =
-  if (even $ (fingers . human $ turn) + (fingers . ai $ turn))
-  then player $  ai turn
-  else player $ human turn
 
-play :: Player -> IO Hand
-play p@Human = Hand p <$> readLn
-play p@AI    = Hand p <$> randomRIO (1, 5)
+sideFromInt :: Int -> Side
+sideFromInt x = if even x then Evens else Odds
+
+winnerSide :: (Turn -> Hand) -> (Turn -> Hand) -> Turn -> Side
+winnerSide p1 p2 = side' . getSum <$> (handFingers p1) `mappend` (handFingers p2)
+  where handFingers hand = Sum <$> fingers . hand
+        side' x = if even x then Evens else Odds
+
+whoPlays :: Side -> Turn -> Player
+whoPlays side' turn = if humanSide turn == side' then human' else ai'
+  where human' = player . human $ turn
+        ai'    = player . ai $ turn
+        humanSide = side . player . human
+
+whoWin :: Turn -> Player
+whoWin inTurn = whoPlays winnerSide' inTurn
+  where winnerSide' = winnerSide human ai inTurn
+
+playHand :: Player -> IO Hand
+playHand p@(AI    _) = Hand p <$> randomRIO (1, 5)
+playHand p@(Human _) = hint *> (Hand p <$> readLn)
+  where hint = putStrLn "Show fingers:"
+
+playTurn :: Side -> IO Turn
+playTurn side = (liftA2 playTurn' (Human) (AI . oposite)) $ side
+  where playTurn' human ai = liftA2 Turn (playHand human) (playHand ai)
 
 congratulate :: Player -> IO ()
-congratulate Human = putStrLn "Congrats Human you win"
-congratulate AI    = putStrLn "Congrats AI you win"
+congratulate player = printWith " " ["Congrats", showPlayer player, "you win"]
+
+showPlayer :: Player -> String
+showPlayer (Human _) = "Human"
+showPlayer (AI _)    = "AI"
 
 showScore :: Turn -> IO ()
-showScore (Turn human ai) = do
-  putStrLn . concat $ ["Human: " , show $ fingers human, " fingers"]
-  putStrLn . concat $ ["AI: " , show $ fingers ai, " fingers"]
+showScore turn = do
+  showHandScore human
+  showHandScore ai
+  where showHandFingers = show . fingers
+        showHandScore hand = printWith " " [showPlayer . player . hand $ turn, showHandFingers . hand $ turn, "fingers"]
+
+chooseSide :: IO Side
+chooseSide = hint *> readLn
+  where hint = putStrLn "Choose side: Odds - Evens?"
+
+oposite :: Side -> Side
+oposite Odds  = Evens
+oposite Evens = Odds
+
+printWith :: String -> [String] -> IO ()
+printWith separator = putStrLn . concat . intersperse separator
+
+play' :: IO ()
+play' = chooseSide >>= playTurn >>= whoWin' >>= congratulate
+  where whoWin' turn = (showScore turn) *> (return . whoWin $ turn)
+
+play :: IO ()
+play = chooseSide' >=> playTurn >=> whoWin' >=> congratulate $ trigger
+  where chooseSide' = const chooseSide
+        whoWin'     = showScore *> return . whoWin
+        trigger     = undefined
 
 main :: IO ()
-main = do
-  human <- play Human
-  ai    <- play AI
-  turn  <- return $ Turn human ai
-  showScore turn
-  congratulate $ winsTurn turn
+main = play'
