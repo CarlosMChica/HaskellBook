a8d13540861e44eba456485fb3d827a00dd6c4a3
Carlos Morera de la Chica <carlos@codurance.com>
2018-04-02 22:56:06 +0100

    Add concept of character - WIP

--- chapter26/Morra.hs
+++ chapter26/Morra.hs
@@ -7,8 +7,19 @@ import           Control.Monad.Trans.State
 import           Data.List
 import           System.Random
 
+data GameMode = GameMode
+  { p1Character :: Character,
+    p2Character :: Character
+  }
+data Character = Human | Computer deriving Show
 data Side = Odds | Evens deriving (Read, Eq)
-data Player = P1 { side :: Side} | P2 { side :: Side}
+data Player = P1 { playerData :: PlayerData} |
+              P2 { playerData :: PlayerData}
+
+data PlayerData = PlayerData {
+    character :: Character,
+    side      :: Side
+  }
 data Hand = Hand {
     player  :: Player,
     fingers :: Int
@@ -17,6 +28,8 @@ data Turn = Turn {
     p1Hand :: Hand,
     p2Hand :: Hand
   }
+
+
 
 winner :: Turn -> Player
 winner = liftA3 playerInSide
@@ -29,16 +42,28 @@ winner = liftA3 playerInSide
         winner count = if even count then Evens else Odds
 
 playerInSide :: Player -> Player -> Side -> Player
-playerInSide p1 p2 side' = if side p1 == side' then p1 else p2
+playerInSide p1@(P1 p1Data) p2 side' = if side p1Data == side' then p1 else p2
 
 playHand :: Player -> IO Hand
-playHand p@(P2    _) = Hand p <$> randomRIO (1, 5)
-playHand p@(P1 _) = hint *> (Hand p <$> readLn)
+playHand = abc
   where hint = putStr "Show fingers: "
+        abc player = case character . playerData $ player of
+          Human    -> hint *> (Hand player <$> readLn)
+          Computer -> Hand player <$> randomRIO (1, 5)
 
-playTurn :: Side -> IO Turn
-playTurn = (liftA2 playTurn' P1 (P2 . oposite))
+playTurn :: GameMode -> Side -> IO Turn
+playTurn mode = liftA2 playTurn'
+                       (P1 . PlayerData (p1Character mode))
+                       (P2 . PlayerData (p2Character mode) . oposite)
   where playTurn' p1 p2 = liftA2 Turn (playHand p1) (playHand p2)
+
+chooseGameMode :: IO GameMode
+chooseGameMode = do
+  putStr "1.- Human-Human * 2.- Human-Computer"
+  mode <- (readLn :: IO Int)
+  case mode of
+    1 -> return $ GameMode Human Human
+    2 -> return $ GameMode Human Computer
 
 chooseSide :: IO Side
 chooseSide = hint *> readLn
@@ -58,15 +83,8 @@ data GameState = GameState
   }
 
 incrementScore :: Player -> GameState -> GameState
-incrementScore (P1 _) game    = game { p1Score = (p1Score game) + 1}
-incrementScore (P2    _) game = game { p2Score = (p2Score game) + 1}
-
-play :: Side -> StateT GameState IO ()
-play p1Side = do
-  turn <- liftIO $ playTurn p1Side
-  liftIO $ showTurn turn
-  modify . incrementScore . winner $ turn
-  get >>= liftIO . showScore
+incrementScore (P1 _) game = game { p1Score = (p1Score game) + 1}
+incrementScore (P2 _) game = game { p2Score = (p2Score game) + 1}
 
 showScore :: GameState -> IO ()
 showScore game =
@@ -86,24 +104,32 @@ showTurn turn = do
           p2Fingers = fingers . p2Hand $ turn
           totalFingers = p1Fingers + p2Fingers
 
+play :: GameMode -> Side -> StateT GameState IO ()
+play mode p1Side = do
+  turn <- liftIO $ playTurn mode p1Side
+  liftIO $ showTurn turn
+  modify . incrementScore . winner $ turn
+  get >>= liftIO . showScore
+
 continue :: IO Bool
 continue = do
   putStrLn "Continue?"
   readLn
 
-repeatGame  :: Side -> IO ()
-repeatGame p1Side = go (GameState 0 0)
-  where go game = do
-           nextState <- liftIO $ execStateT (play p1Side) game
+repeatGame  :: GameMode -> Side -> IO ()
+repeatGame mode p1Side = go (GameState 0 0)
+  where go gameState = do
+           nextState <- liftIO $ execStateT (play mode p1Side) gameState
            cont <- liftIO continue
            if cont then (go nextState) else return ()
 
 main' :: IO ()
 main' = do
+  mode <- chooseGameMode
   p1Side <- chooseSide
-  repeatGame p1Side
+  repeatGame mode p1Side
 
 main :: IO ()
 main = do
   evalStateT (mapM_ play' [(1 :: Int)..]) $ GameState 0 0
-  where play' = const (liftIO chooseSide >>= play)
+  where play' = const $ liftA2 play (liftIO chooseGameMode) (liftIO chooseSide)
