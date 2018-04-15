4e219e557359d5aa5de4cd2e16dbb3b00b4daa3a
Carlos Morera de la Chica <carlos@codurance.com>
2018-04-15 19:55:21 +0100

    Some refactors

--- chapter26/Morra.hs
+++ chapter26/Morra.hs
@@ -15,6 +15,7 @@ import           Control.Applicative
 import           Control.Monad
 import           Control.Monad.IO.Class
 import           Control.Monad.Trans.State.Strict
+import           Data.Bool
 import           Data.List
 import           Data.Monoid
 import           System.Random
@@ -24,6 +25,7 @@ data Character = Human | Computer deriving (Eq, Show)
 data Side = Odds | Evens deriving (Read, Eq, Show)
 data Player = P1 { playerData :: PlayerData} |
               P2 { playerData :: PlayerData} deriving Show
+
 
 data PlayerData = PlayerData {
     character :: Character,
@@ -53,21 +55,27 @@ winner = liftA3 playerInSide
                 p1
                 p2
                 (winnerSide . totalFingers . last . turns)
-  where totalFingers :: Turn -> Int
-        totalFingers = liftA2 (+) p1Fingers p2Fingers
+  where totalFingers = liftA2 (+) p1Fingers p2Fingers
         winnerSide count = if even count then Evens else Odds
+        playerInSide pl1 pl2 side' = if (side . playerData $ pl1) == side' then pl1 else pl2
+
+boolf :: (a -> Bool) -> (a -> b) -> (a -> b) -> a -> b
+boolf h f g x = if h x then f x else g x
+
+eqf :: Eq b => (a -> b) -> (a -> b) -> a -> Bool
+eqf f g x = f x == g x
 
 playComputerHand :: GameState -> IO Int
-playComputerHand gameState = if canPlaySmart humanMoves then playComputerHandSmart humanMoves else randomRIO (1,5)
-  where humanMoves = moves . playerData . p1 $ gameState
-        canPlaySmart humanMoves | length humanMoves < 4 = False
-        canPlaySmart humanMoves = first2HumanMoves == last2HumanMoves
-          where last2HumanMoves = [last . init $ humanMoves, last humanMoves]
-                first2HumanMoves = take 2 humanMoves
-        playComputerHandSmart p1Moves = do
+playComputerHand = boolf canPlaySmart playSmart playRandom . humanMoves
+  where humanMoves = moves . playerData . p1
+        canPlaySmart = boolf ((>4) <$> length) (first2HumanMoves `eqf` last2HumanMoves) (const False)
+          where last2HumanMoves = sequence [last . init, last]
+                first2HumanMoves = take 2
+        playSmart p1Moves = do
           smartPlay <- return $ last . take 3 $ p1Moves
           putStrLnWith " " ["Playing smart.", "Played ", show smartPlay]
           return smartPlay
+        playRandom = const $ randomRIO (1,5)
 
 playHumanHand :: Player -> IO Int
 playHumanHand player = do
@@ -110,15 +118,17 @@ putStrLnWith = printWith putStrLn
 printWith :: (String -> IO ()) -> String -> [String] -> IO ()
 printWith f separator = f . concat . intersperse separator
 
-playerInSide :: Player -> Player -> Side -> Player
-playerInSide p1@(P1 p1Data) p2 side' = if side p1Data == side' then p1 else p2
-playerInSide p2@(P2 p2Data) p1 side' = if side p2Data == side' then p2 else p1
-
 incrementScore :: Player -> GameState -> GameState
-incrementScore (P1 _) (GameState m pl1 pl2 ts) = GameState m (incrementP1Score pl1) pl2 ts
-  where incrementP1Score (P1 (PlayerData char moves score side)) = P1 $ PlayerData char moves (score + 1) side
-incrementScore (P2 _) (GameState m pl1 pl2 ts) = GameState m pl1 (incrementP2Score pl2) ts
-  where incrementP2Score (P2 (PlayerData char moves score side)) = P2 $ PlayerData char moves (score + 1) side
+incrementScore (P1 _) (GameState m pl1 pl2 ts) = GameState m (incrementPScore pl1) pl2 ts
+incrementScore (P2 _) (GameState m pl1 pl2 ts) = GameState m pl1 (incrementPScore pl2) ts
+
+
+incrementPScore :: Player -> Player
+incrementPScore player = player {
+                           playerData = (playerData player) {
+                                score = (score (playerData player)) + 1
+                                }
+                           }
 
 addTurn :: Turn -> GameState -> GameState
 addTurn t (GameState m pl1 pl2 ts) = GameState m pl1 pl2 (ts ++ [t])
