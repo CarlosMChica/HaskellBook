0296ea066bef7ac1ad7374671dfaf41b6e5714ad
Carlos Morera de la Chica <carlos@codurance.com>
2018-05-16 21:53:53 +0100

    Add file IO to vigenere cypher chapter29

--- chapter29/cypher/app/Main.hs
+++ chapter29/cypher/app/Main.hs
@@ -1,11 +1,17 @@
 module Main where
 
+import System.IO
+import System.Exit
+import Control.Monad
+import System.Environment (getArgs)
 import Lib
 
+main :: IO ()
 main = do
-  putStr "Enter the message: "
-  message <- getLine
-  putStr "Enter the ciphering word: "
-  word <- getLine
-  putStrLn $ "Ciphered message: " ++ vigenere message word
---"MEET AT DAWN" "ALLY" == "MPPR AE OYWY"
+  args <- getArgs
+  case args of
+    [mode, key] ->
+      case mode of
+        "-d" -> hPutStrLn stdout =<< (flip unVigenere key) <$> hGetLine stdin
+        "-e" -> hPutStrLn stdout =<< (flip vigenere key) <$> hGetLine stdin
+        otherwise -> hPutStrLn stderr "Unkown option" >> exitFailure
